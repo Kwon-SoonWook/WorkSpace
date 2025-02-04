503380df08e49a5c2e1414246483dd816c7cfb7f
Kwon-SoonWook <tnsdnrzz@naver.com>
2025-02-04 17:52:23 +0900

    123

--- bookManagements/src/bookManagements/BookManagement.java
+++ bookManagements/src/bookManagements/BookManagement.java
@@ -3,7 +3,7 @@ import java.awt.*;
 import java.sql.*;
 import java.awt.event.*;
 public class BookManagement extends Frame implements ActionListener {
-	//DB ¿¬µ¿
+	//DB
 	PreparedStatement ps;
 	ResultSet rs;
 	String sql;
