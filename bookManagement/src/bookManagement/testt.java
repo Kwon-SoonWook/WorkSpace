ba267b54c5602dfa284b58ff201827a446089240
NGJ-une <rhkdwns124@naver.com>
2025-02-05 10:03:26 +0900

    asdasdasd

--- bookManagement/src/bookManagement/testt.java
+++ bookManagement/src/bookManagement/testt.java
@@ -5,12 +5,7 @@ public class testt {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//asdasdasd
-<<<<<<< HEAD
-		
-		int a;
-=======
-		String ddd = "fsdfosdfjasod";
->>>>>>> branch 'develop' of https://github.com/Kwon-SoonWook/WorkSpace.git
+
 	}
 
 }
